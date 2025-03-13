0bc232d8f1a15811c1d5f4474c526d2e077d93e2
gabrielprdg <gabriel.pr07@hotmail.com>
2025-03-12 21:22:06 -0300

    feat: add AuthMiddleware to protect our routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,18 @@
 import { DatabaseModule } from '@infra/database/database.module';
 import { HttpModule } from '@infra/http.module';
-import { Module } from '@nestjs/common';
+import { TransactionController } from '@infra/http/controllers/transaction/transaction.controller';
+import { AuthMiddleware } from '@infra/middlewares/auth-middleware';
+import { AuthService } from '@infra/services/auth.service';
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 @Module({
+  providers: [AuthService],
   imports: [HttpModule, DatabaseModule],
 })
-export class AppModule { }
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AuthMiddleware)
+      .exclude({ path: 'auth/login', method: RequestMethod.POST })
+      .forRoutes(TransactionController);
+  }
+}
